6f5e688b3b59bf62b60b794f8ad7b3303cf204ed
Sholawasiu <sholawasiu92@gmail.com>
2024-08-15 07:46:49 -0700

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+# 0-subs.py
+
+Python script queries the Reddit API and returns the number of subscribers.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Main function
+    """
+    url = 'https://reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'tonybnya'}
+    res = requests.get(url, headers=headers)
+    subscribers = res.json().get('data', {}).get('subscribers', 0)
+
+    return subscribers
